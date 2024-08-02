bdad9dbe4dca6d062badda3277a911915ddf528f
Ennimex <alexanderhernan325@gmail.com>
2024-08-02 12:38:10 -0600

    ValidacionesV5

--- Vistas/Publica/frmlogin.php
+++ Vistas/Publica/frmlogin.php
@@ -31,27 +31,27 @@
             <form action="/restaurante/index?clase=controladorpublico&metodo=register" method="POST">
                 <div class="input-box">
                     <span class="icon"><ion-icon name="person"></ion-icon></span>
-                    <input type="text" name="txtNombre" id="" required pattern="^[a-zA-Z\s]+$" title="El nombre solo debe contener letras y espacios.">
+                    <input type="text" name="txtNombre" id="" required pattern="^[a-zA-Z\s]+$" title="El nombre solo debe contener letras y espacios." maxlength="20">
                     <label>Nombre</label>
                 </div>
                 <div class="input-box">
                     <span class="icon"><ion-icon name="person"></ion-icon></span>
-                    <input type="text" name="txtApellido" id="" required pattern="^[a-zA-Z\s]+$" title="Los apellidos solo deben contener letras y espacios.">
+                    <input type="text" name="txtApellido" id="" required pattern="^[a-zA-Z\s]+$" title="Los apellidos solo deben contener letras y espacios." maxlength="20">
                     <label>Apellidos</label>
                 </div>
                 <div class="input-box">
                     <span class="icon"><ion-icon name="call-outline"></ion-icon></span>
-                    <input type="tel" name="txtTelefono" required pattern="^\d{10}$" title="El número de teléfono debe contener 10 dígitos.">
+                    <input type="tel" name="txtTelefono" required pattern="^\d{10}$" title="El número de teléfono debe contener 10 dígitos." maxlength="13">
                     <label>Teléfono</label>
                 </div>
                 <div class="input-box">
                     <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
-                    <input type="email" name="txtEmail" id="" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Por favor ingrese un correo válido.">
+                    <input type="email" name="txtEmail" id="" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Por favor ingrese un correo válido." maxlength="25">
                     <label>Correo</label>
                 </div>
                 <div class="input-box">
                     <span class="icon"><ion-icon name="bag-outline"></ion-icon></span>
-                    <input type="password" name="txtPassword" id="" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula y un número.">
+                    <input type="password" name="txtPassword" id="" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula y un número." maxlength="25">
                     <label>Contraseña</label>
                 </div>
                 <div class="remember-forgot">
